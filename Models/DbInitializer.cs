3ce9cbff67d01f7f9dc8c00a865b91344d0bcb3a
tshimsthedoor <tshims79@gmail.com>
2024-01-08 20:36:41 +0200

    Add project files.

---
+++ Models/DbInitializer.cs
@@ -0,0 +1,39 @@
+﻿using TodoApi.Data;
+
+namespace TodoApi.Models
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(TodoContext context) 
+        {
+            context.Database.EnsureCreated();
+            // look for any tables
+            if (context.TodoItems.Any())
+            {
+                return;
+            }
+
+            // Seed the database with some test data
+            var todos = new TodoItem[]
+            {
+                new TodoItem
+                {
+                    Name = "Learn C#"
+                },
+                new TodoItem
+                {
+                    Name = "Learn ASP.NET Core"
+                }
+                ,new TodoItem
+                {
+                    Name = "Build a web app"
+                }
+            };
+            foreach (var todo in todos)
+            {
+                context.TodoItems.Add(todo);
+            }
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Data;
+using TodoApi.Models;
+var builder = WebApplication.CreateBuilder(args);
+using var cnn = new SqliteConnection("Filename=:memory:");
+cnn.Open();
+builder.Services.AddDbContext<TodoContext>(o => o.UseSqlite(cnn));
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+var scope = app.Services.CreateScope();
+var db = scope.ServiceProvider.GetService<TodoContext>();
+//DbInitializer.Initialize(db);
+
+DbInitializer.Initialize(db);
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+}
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapControllerRoute(
+        name: "GetTodoItem",
+        pattern: "api/todoitems/{id}",
+        defaults: new { controller = "TodoItems", action = "GetTodoItemAsync" }
+    );
+});
+
+
+
+app.Run();
+
+public partial class Program
+{
+
+}
